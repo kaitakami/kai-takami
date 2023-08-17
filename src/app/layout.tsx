ed3e31962d8f905cb59a4ee8db349c29845c27b5
Kai Takami <kaitakami@protonmail.com>
2023-08-16 22:54:33 -0600

    feat: add container class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} max-w-2xl m-auto p-3 py-20`}>{children}</body>
     </html>
   )
 }
