09636bf10455d091e1397c9bf47b566bb57e474a
Kai Takami <kaitakami@protonmail.com>
2023-10-17 22:15:21 -0600

    chore: add style to blog website

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="flex flex-col gap-5">
+      <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">Blog</h1>
       <p>Here I share content related with my personal life, startups, and code.</p>
     </div>
   )
